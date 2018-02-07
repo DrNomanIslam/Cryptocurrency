d969a84ee6c5be2a441390a027c1e7bba98984dd
Noman <noman.islam@hotmail.com>
2018-02-08 00:22:53 +0500

    Final Changes

--- task1/task1_Step1.py
+++ task1/task1_Step1.py
@@ -1,5 +1,6 @@
 import requests
 
+#Calculates the current price for currency (symbol) in comparison to USD or any provided currency
 def price(symbol, comparison_symbols=['USD'], exchange=''):
     url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
             .format(symbol.upper(), ','.join(comparison_symbols).upper())
