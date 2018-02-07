3d80cd5746c5d57817f5a8204f51e5ccf75e033d
Noman <noman.islam@hotmail.com>
2018-02-08 00:19:11 +0500

    Task1

---
+++ task1/task1_Step1.py
@@ -0,0 +1,13 @@
+import requests
+
+def price(symbol, comparison_symbols=['USD'], exchange=''):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
+            .format(symbol.upper(), ','.join(comparison_symbols).upper())
+    if exchange:
+        url += '&e={}'.format(exchange)
+    page = requests.get(url)
+    data = page.json()
+    return data
+
+#print("The current price of BTC in USD=", price('BTC'))
+#print("The current price of ETH in USD=", price('ETH'))
